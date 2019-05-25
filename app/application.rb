8eb7bde5a4bc5b39b00e0f976a16aae6bf858c7c
matthew.serre <matthew.serre@gmail.com>
2019-05-25 23:45:13 +0000

    Automatically backed up by Learn

--- app/application.rb
+++ app/application.rb
@@ -15,10 +15,10 @@ class Application
     elsif req.path.match(/search/)
       search_term = req.params["q"]
       resp.write handle_search(search_term)
-    elsif req.path.match(/cart/)
-      @@cart.each do |item|
-        resp.write "#{item}\n"
-    # elsif req.path.match(/add/)
+    # elsif req.path.match(/cart/)
+    #   @@cart.each do |item|
+    #     resp.write "#{item}\n"
+    # # elsif req.path.match(/add/)
     else
       resp.write "Path Not Found"
     end
