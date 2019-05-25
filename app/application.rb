ce9aa5a08b761bb0d0a54f4781e525b3d5db5364
matthew.serre <matthew.serre@gmail.com>
2019-05-25 23:43:47 +0000

    Automatically backed up by Learn

--- app/application.rb
+++ app/application.rb
@@ -18,6 +18,7 @@ class Application
     elsif req.path.match(/cart/)
       @@cart.each do |item|
         resp.write "#{item}\n"
+    elsif req.path.match(/add/)
     else
       resp.write "Path Not Found"
     end
